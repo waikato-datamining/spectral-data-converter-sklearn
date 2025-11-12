79f1e8bb17893f78d13bb9e4874b5f655a0575d5
Peter Reutemann <fracpete@gmail.com>
2025-11-13 10:15:21 +1300

    removed "License" classifier (deprecated)

--- setup.py
+++ setup.py
@@ -21,7 +21,6 @@ setup(
     url="https://github.com/waikato-datamining/spectral-data-converter-sklearn",
     classifiers=[
         'Development Status :: 4 - Beta',
-        'License :: OSI Approved :: MIT License',
         'Topic :: Scientific/Engineering',
         'Topic :: Scientific/Engineering :: Artificial Intelligence',
     ],
